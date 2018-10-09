ac9659bab2d3f4e59c73a02ec80b1fc1b2bef9be
jinxy <17611268055@163.com>
2018-10-09 19:21:41 +0800

    进晓宇必须是一个好娃娃

--- app-abd/src/com/asiainfo/abdinfo/testHql/Test.java
+++ app-abd/src/com/asiainfo/abdinfo/testHql/Test.java
@@ -3,6 +3,8 @@ package com.asiainfo.abdinfo.testHql;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("进小宇是个好娃娃");
+		System.out.println("进晓宇必须是一个好娃娃");
+		
 	}
 
 }
