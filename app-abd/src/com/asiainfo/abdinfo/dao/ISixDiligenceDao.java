aa79a3423cfbb4c217b20090c4ede124f2fedafb
jinxy <17611268055@163.com>
2018-10-09 18:06:26 +0800

    进进晓宇到此一游

--- app-abd/src/com/asiainfo/abdinfo/dao/ISixDiligenceDao.java
+++ app-abd/src/com/asiainfo/abdinfo/dao/ISixDiligenceDao.java
@@ -8,6 +8,11 @@ public interface ISixDiligenceDao {
 	
 	//查询六项精进打卡内容
 	public SixDiligence findSixDiligence(Map<String, Object> map);
+	/**
+	 * 进晓宇到此一游
+	 * @param map
+	 * @return
+	 */
 	public SixDiligence findTips(Map<String, Object> map);
 	public SixDiligence findReadFeeling(Map<String, Object> map);
 	public SixDiligence findWork(Map<String, Object> map);
