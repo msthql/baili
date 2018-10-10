d25acd7195a02ff6137169d5612b12bac2d0e4e8
Administrator <Administrator@SKYUSER-JIQP4CG>
2018-10-10 09:56:55 +0800

    王海成    是一个好娃娃  2018-10-10

--- app-abd/src/com/asiainfo/abdinfo/testHql/Test.java
+++ app-abd/src/com/asiainfo/abdinfo/testHql/Test.java
@@ -1,9 +1,12 @@
 package com.asiainfo.abdinfo.testHql;
 
+import static org.hamcrest.CoreMatchers.sameInstance;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("进小宇是个好娃娃");
 		System.out.println("进晓宇必须是一个好娃娃");
+		System.out.println("王海成是一个好娃娃");
 		
 	}
 
